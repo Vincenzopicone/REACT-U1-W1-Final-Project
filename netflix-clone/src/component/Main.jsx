045c0f87db68c2024cdb7dd02fed5aea3adf9ab1
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-02-10 13:01:58 +0100

    Add Navbar e Footer

---
+++ netflix-clone/src/component/Main.jsx
@@ -0,0 +1,18 @@
+import Galleria1 from "./Galleria1";
+import Galleria2 from "./Galleria2";
+import Galleria3 from "./Galleria3";
+/* import Section from "./Section"; */
+
+
+const Main = () => (
+    <main>
+        <div class="container-fluid px-4">
+           {/* <Section/> */}      
+           <Galleria1/>
+           <Galleria2/>
+           <Galleria3/>
+        </div>
+    </main>
+)
+
+export default Main;
