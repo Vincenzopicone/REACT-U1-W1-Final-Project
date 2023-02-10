dee813bfbf2300a42d4b6fa8f69eb2a1661629e1
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-02-10 13:21:07 +0100

    Create component

---
+++ netflix-clone/src/component/Galleria2.jsx
@@ -0,0 +1,29 @@
+const Galleria2 = () => (
+    <>
+    <h4>Watch it Again</h4>
+        <div
+          className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center"
+        >
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/7.png"} alt={"movie picture"} />
+          </div>
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/8.png"} alt={"movie picture"} />
+          </div>
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/9.png"} alt={"movie picture"} />
+          </div>
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/10.png"} alt={"movie picture"} />
+          </div>
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/11.png"} alt={"movie picture"} />
+          </div>
+          <div className="col mb-2 px-1">
+            <img className="img-fluid" src={"./assets/12.png"} alt={"movie picture"} />
+          </div>
+        </div>
+    </>
+);
+
+export default Galleria2;
--- netflix-clone/src/component/Main.jsx
+++ netflix-clone/src/component/Main.jsx
@@ -1,18 +1,16 @@
 import Galleria1 from "./Galleria1";
 import Galleria2 from "./Galleria2";
 import Galleria3 from "./Galleria3";
-/* import Section from "./Section"; */
+import Section from "./Section";
 
 
 const Main = () => (
-    <main>
         <div class="container-fluid px-4">
-           {/* <Section/> */}      
+          <Section/>    
            <Galleria1/>
            <Galleria2/>
            <Galleria3/>
         </div>
-    </main>
 )
 
 export default Main;
