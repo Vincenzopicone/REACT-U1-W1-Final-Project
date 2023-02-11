1c918593f9bdeb8e9fe0d6771672e28a4cee0ddc
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-02-11 12:53:19 +0100

    Add Spinner add Alert error

--- netflix-clone/src/component/Galleria.jsx
+++ netflix-clone/src/component/Galleria.jsx
@@ -1,10 +1,11 @@
 import { Component } from "react";
-/* import { Image } from "react-bootstrap"; */
+import {Spinner} from "react-bootstrap"
 import CardFilm from "./CardFilm";
 
 class Galleria extends Component {
     state = {
-        Search:[]
+        Search:[],
+        load: true
     }
 
     fetchFilm = async () => {
@@ -14,11 +15,17 @@ class Galleria extends Component {
             if (response.ok) {
                 const data = await response.json();
                 this.setState({
-                    "Search": data.Search})
-            } else {             
+                    "Search": data.Search,
+                    "load": false
+                })
+            } else {
+                this.setState({"load":false})
+                alert("Vai sul vero Netflix e non perdere tempo qui")            
             }
         } catch (error) {
-            console.log("Errore caricamento", error)
+            this.setState({"load":false})
+            alert("Vai sul vero Netflix e non perdere tempo qui")
+            
         }
     }
 
@@ -30,8 +37,13 @@ class Galleria extends Component {
         
         return (
             <div className="d-flex justify-content-evenly align-items-center flex-wrap">
+                {this.state.load && (
+              <div className="d-flex justify-content-center">
+                <Spinner animation="border" variant="danger" />
+              </div>
+            )}
           {this.state.Search.filter(film=> film.Type==="movie" && film.Year > 2000)
-          .map(film => ( <CardFilm key={film.imdbID} img={film.Poster} title={film.Title} year={film.Year} imdbID={film.imdbID} /> ))}        
+          .map(film => (<CardFilm key={film.imdbID} img={film.Poster} title={film.Title} year={film.Year} imdbID={film.imdbID} /> ))}        
         </div>)
         
     }
